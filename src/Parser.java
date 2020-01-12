8dac2d16ccc0915100f1bc9a17d84e5cd9f0cf2a
Satrajit Chatterjee <satrajit2016@gmail.com>
2020-01-12 02:51:39 -0500

    parser completely works

--- src/Parser.java
+++ src/Parser.java
@@ -11,15 +11,20 @@ public class Parser {
 
     public Parser() throws IOException {
 
+        String urlString = "https://github.com/MLH/mlh-hackathon-blockstack-starter?utm_source=Major+League+Hacking+-+2020+Season&utm_campaign=cf20aa51c8-EYNTK_2020&utm_medium=email&utm_term=0_b490f2beb8-cf20aa51c8-9681793";
+
         String content = null;
         URLConnection connection = null;
+
         try {
-            connection =  new URL("https://en.wikipedia.org/wiki/ASDF").openConnection();
+
+            connection =  new URL(urlString).openConnection();
             Scanner scanner = new Scanner(connection.getInputStream());
             scanner.useDelimiter("\\Z");
             content = scanner.next();
             scanner.close();
-        }catch ( Exception ex ) {
+
+        } catch ( Exception ex ) {
             ex.printStackTrace();
         }
 
@@ -33,7 +38,15 @@ public class Parser {
 //        System.out.println(cleanHtml);
 
         String plainText= Jsoup.parse(content).text();
-        System.out.println(plainText);
+
+        try {
+            int requiredIndex = plainText.indexOf("Retrieved from");
+            plainText = plainText.substring(0, requiredIndex);
+        } catch (Exception e) {
+
+        }
+
+        System.out.println(plainText); // this needs to be filtered
 
     }
 
