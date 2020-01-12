987cf81654c0d433af6cffec01fac8f9f977f512
Matthew Ng <mng07@uoguelph.ca>
2020-01-12 02:51:31 -0500

    commit

--- src/webcrawler.py
+++ src/webcrawler.py
@@ -5,24 +5,28 @@ from urllib.request import urlopen
  
 if len(sys.argv) > 1:
 	term = ' '.join(sys.argv[1:])
+	#creates list of url
 	urllist = []
 	finalurl = []
+	#loops through the array, 
 	for url in search(term, tld='com', stop=10):
-		if("ebay.com" not in url and "youtube.com" not in url and "pintrest.com" not in url and "spotify.com" not in url and "twitter.com" not in url and "facebook.com" not in url and "amazon.com" not in url and "amazon.ca" not in url and "reddit.com" not in url):
+		if("ebay.com" not in url and "youtube.com" not in url and "pinterest.com" not in url and "spotify.com" not in url and "twitter.com" not in url and "facebook.com" not in url and "amazon.com" not in url and "amazon.ca" not in url and "reddit.com" not in url):
 			if(term in url):
 				print(url)
 				urllist.append(url)
-	print(urllist)
+	#print(urllist)
+	print("------------------------------------------------------------------------------------------------")
 
 	for i in urllist:
 		soup = BeautifulSoup(urlopen(i), features="lxml")
 		for link in soup.find_all('a'):
 			word = link.get('href')
-			if("ebay.com" not in word and "youtube.com" not in word and "pintrest.com" not in word and "spotify.com" not in word and "twitter.com" not in word and "facebook.com" not in word and "amazon.com" not in word and "amazon.ca" not in word and "reddit.com" not in word and word.startswith("http")):
+			if("ebay.com" not in word and "youtube.com" not in word and "pinterest.com" not in word and "spotify.com" not in word and "twitter.com" not in word and "facebook.com" not in word and "amazon.com" not in word and "amazon.ca" not in word and "reddit.com" not in word and word.startswith("http")):
 				if(term in word):
 	 				finalurl.append(link.get('href'))
+	 				print(word)
 
-	print(finalurl)
+	#print(finalurl)
 
 else:
 	sys.exit('list of items not long enough')
